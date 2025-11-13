c0d15a0318ea536f87c8b2067a304becc5262fe7
Tangyuan <2939056298@qq.com>
2025-11-13 15:58:02 +0800

    fix: Update base path for vue-rabbits-shop repository

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 // https://vite.dev/config/
 export default defineConfig({
   // GitHub Pages 部署配置
-  base: process.env.NODE_ENV === 'production' ? '/vue-rabbits/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/vue-rabbits-shop/' : '/',
   plugins: [
     vue(),
     vueDevTools(),
